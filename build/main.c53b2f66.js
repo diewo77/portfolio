3bb60a4e364897ee1c39e54e9702a1a6eb174475
Takieddine Boumerah <boume.taki@gmail.com>
2018-05-10 22:57:44 +0100

    V0.01

---
+++ build/main.c53b2f66.js
@@ -0,0 +1,6 @@
+parcelRequire=function(e,r,n,t){function i(n,t){function o(e){return i(o.resolve(e))}function c(r){return e[n][1][r]||r}if(!r[n]){if(!e[n]){var l="function"==typeof parcelRequire&&parcelRequire;if(!t&&l)return l(n,!0);if(u)return u(n,!0);if(f&&"string"==typeof n)return f(n);var p=new Error("Cannot find module '"+n+"'");throw p.code="MODULE_NOT_FOUND",p}o.resolve=c;var a=r[n]=new i.Module(n);e[n][0].call(a.exports,o,a,a.exports,this)}return r[n].exports}function o(e){this.id=e,this.bundle=i,this.exports={}}var u="function"==typeof parcelRequire&&parcelRequire,f="function"==typeof require&&require;i.isParcelRequire=!0,i.Module=o,i.modules=e,i.cache=r,i.parent=u;for(var c=0;c<n.length;c++)i(n[c]);if(n.length){var l=i(n[n.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):t&&(this[t]=l)}return i}({17:[function(require,module,exports) {
+
+},{"./../images/art.jpg":[["art.91acaaaa.jpg",18],18]}],8:[function(require,module,exports) {
+"use strict";require("./main.scss"),console.log("Hello");
+},{"./main.scss":17}]},{},[8], null)
+//# sourceMappingURL=/main.04a0cb9b.map
